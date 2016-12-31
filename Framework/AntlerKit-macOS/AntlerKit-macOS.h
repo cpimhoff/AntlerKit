a7ea6081dd4091e5172a376a8a09b529d4791470
Charlie Imhoff <cpimhoff@gmail.com>
2016-12-31 15:07:33 -0500

    Framework Project initial commit

---
+++ Framework/AntlerKit-macOS/AntlerKit-macOS.h
@@ -0,0 +1,19 @@
+//
+//  AntlerKit-macOS.h
+//  AntlerKit-macOS
+//
+//  Created by Charlie Imhoff on 12/31/16.
+//  Copyright © 2016 Charlie Imhoff. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AntlerKit-macOS.
+FOUNDATION_EXPORT double AntlerKit_macOSVersionNumber;
+
+//! Project version string for AntlerKit-macOS.
+FOUNDATION_EXPORT const unsigned char AntlerKit_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AntlerKit_macOS/PublicHeader.h>
+
+
