677bded918c81f26358ab6dc64ae7d3ec1dff17d
Charlie Imhoff <cpimhoff@gmail.com>
2017-01-01 14:40:44 -0500

    some utilities for dealing with type information difference across frameworks

---
+++ Framework/AntlerKit/AddingNumbers.swift
@@ -0,0 +1,50 @@
+//
+//  AddingNumbers.swift
+//  AntlerKit
+//
+//  Created by Charlie Imhoff on 1/1/17.
+//  Copyright © 2017 Charlie Imhoff. All rights reserved.
+//
+
+import Foundation
+
+func +(lhs: Float, rhs: CGFloat) -> Float {
+	return lhs + Float(rhs)
+}
+func +(lhs: CGFloat, rhs: Float) -> CGFloat {
+	return lhs + CGFloat(rhs)
+}
+func +=(lhs: inout Float, rhs: CGFloat) {
+	lhs = lhs + rhs
+}
+func +=(lhs: inout CGFloat, rhs: Float) {
+	lhs = lhs + rhs
+}
+
+func -(lhs: Float, rhs: CGFloat) -> Float {
+	return lhs - Float(rhs)
+}
+func -(lhs: CGFloat, rhs: Float) -> CGFloat {
+	return lhs - CGFloat(rhs)
+}
+func -=(lhs: inout Float, rhs: CGFloat) {
+	lhs = lhs - rhs
+}
+func -=(lhs: inout CGFloat, rhs: Float) {
+	lhs = lhs - rhs
+}
+
+
+func /(lhs: Float, rhs: CGFloat) -> Float {
+	return lhs / Float(rhs)
+}
+func /(lhs: CGFloat, rhs: Float) -> CGFloat {
+	return lhs / CGFloat(rhs)
+}
+
+func *(lhs: Float, rhs: CGFloat) -> Float {
+	return lhs * Float(rhs)
+}
+func *(lhs: CGFloat, rhs: Float) -> CGFloat {
+	return lhs * CGFloat(rhs)
+}
